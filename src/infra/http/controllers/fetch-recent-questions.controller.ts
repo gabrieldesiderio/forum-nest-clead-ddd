a46843fc1af751d69b2ba727ddb30809751d9901
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-09-29 12:15:05 -0300

    test: refact e2e tests and create factories

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
-import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { QuestionPresenter } from '../presenters/question-presenter'
@@ -13,16 +13,14 @@ const pageQueryParamSchema = z
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 
-type PageQueryParamsSchema = z.infer<typeof pageQueryParamSchema>
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 @Controller('/questions')
 export class FetchRecentQuestionsController {
   constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
   @Get()
-  async handle(
-    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
-  ) {
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
     const result = await this.fetchRecentQuestions.execute({
       page,
     })
